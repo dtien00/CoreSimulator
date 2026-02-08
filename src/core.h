93895c1cb43de23096c6decff0c66c33eb6ca89a
dtien00 <dtien00@gmaile.com>
2026-02-07 21:20:51 -0500

    Initial implementation of core object

--- src/core.h
+++ src/core.h
@@ -1,1 +1,21 @@
 #include "scheduler.h"
+
+#ifndef CORE
+#define CORE
+typedef struct {
+    Scheduler sch; // Scheduler associated with the core
+    int id; // Core ID
+} Core;
+
+#endif CORE
+
+void runScheduler(int protocol){
+    /* Based on the protocol parameter value, runs a specific scheduler behavior */
+    /* @param protocol: Determines Scheduler object behavior to be performed 
+        - 1: addProcess
+        - 2: removeProcess
+        - 3: runProcess
+    */
+
+    
+}
--- src/main.c
+++ src/main.c
@@ -10,3 +10,11 @@ int main(){
 
     return 0;
 }
+
+Core* createCore(int id){
+    /* Creates and returns pointer to a newly initialized Core object */
+    Core init;
+    init.id = id;
+
+    return &init;
+}
